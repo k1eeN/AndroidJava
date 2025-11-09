1ebf9b55245f479805d9d8e7c6aca4b62ff22b71
Mark Klinitskii <mark23klin@mail.ru>
2025-11-09 18:31:15 +0300

    Lesson 2 - Color Descriptor

--- app/src/main/java/com/example/androidjava/MainActivity.java
+++ app/src/main/java/com/example/androidjava/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidjava;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner spinnerColors;
+    private TextView textViewDescriptionTemp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        spinnerColors = findViewById(R.id.spinnerColors);
+        textViewDescriptionTemp = findViewById(R.id.textViewDescriptionTemp);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void showDescription(View view) {
+        int pos = spinnerColors.getSelectedItemPosition();
+        String description = getDescriptionByPos(pos);
+        textViewDescriptionTemp.setText(description);
+    }
+
+    private String getDescriptionByPos(int pos) {
+        String[] description = getResources().getStringArray(R.array.dis_color);
+        return description[pos];
     }
 }
