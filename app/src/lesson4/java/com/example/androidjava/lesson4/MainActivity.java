7c607f0f90e7c3a19a48aaacf2aa8157db5ac0f0
Mark Klinitskii <mark23klin@mail.ru>
2025-11-10 18:50:30 +0300

    Lesson 4 - Timer

--- app/src/lesson4/java/com/example/androidjava/lesson4/MainActivity.java
+++ app/src/lesson4/java/com/example/androidjava/lesson4/MainActivity.java
@@ -1,11 +1,12 @@
-package com.example.androidjava.lesson5;
+package com.example.androidjava.lesson4;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,18 +14,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.androidjava.R;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner spinnerColors;
-    private TextView textViewDescriptionTemp;
+    private TextView textViewTimer;
+
+    private int seconds = 0;
+    private boolean isRunning = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        spinnerColors = findViewById(R.id.spinnerColors);
-        textViewDescriptionTemp = findViewById(R.id.textViewDescriptionTemp);
+        textViewTimer = findViewById(R.id.textViewTimer);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            isRunning = savedInstanceState.getBoolean("isRunning");
+        }
+        runTimer();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -32,14 +43,49 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void showDescription(View view) {
-        int pos = spinnerColors.getSelectedItemPosition();
-        String description = getDescriptionByPos(pos);
-        textViewDescriptionTemp.setText(description);
+    @Override
+    protected void onStop() {
+        super.onStop();
+
     }
 
-    private String getDescriptionByPos(int pos) {
-        String[] description = getResources().getStringArray(R.array.dis_color);
-        return description[pos];
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("seconds", seconds);
+        outState.putBoolean("isRunning", isRunning);
+    }
+
+    public void onClickStartTimer(View view) {
+        isRunning = true;
+    }
+
+    public void onClickPauseTimer(View view) {
+        isRunning = false;
+    }
+
+    public void onClickRestartTimer(View view) {
+        isRunning = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours,minutes,secs);
+                textViewTimer.setText(time);
+
+                if (isRunning) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
