8e58f2a2c6070afebc8dc178a437a271520effcc
Mark Klinitskii <mark23klin@mail.ru>
2025-11-15 00:33:33 +0300

    Lesson 8 - Todo List - Room Database - NotesDao

---
+++ app/src/lesson8TodoList/java/com/example/androidjava/lesson_8_todo_list/NoteDao.java
@@ -0,0 +1,23 @@
+package com.example.androidjava.lesson_8_todo_list;
+
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Query("SELECT * FROM notes")
+    List<Note> getNotes();
+
+    @Insert
+    void add(Note note);
+
+    @Query("DELETE FROM notes WHERE id = :id")
+    void remove(int id);
+}
