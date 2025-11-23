82450224822914aeb285ef2d7e061bd43bf9f7dd
Mark Klinitskii <mark23klin@mail.ru>
2025-11-23 19:20:30 +0300

    Lesson 12 - Messenger - ChatActivity and Design Activity

---
+++ app/src/lesson12Messenger/java/com/example/androidjava/lesson_12_messenger/ChatActivity.java
@@ -0,0 +1,48 @@
+package com.example.androidjava.lesson_12_messenger;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidjava.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private TextView textViewTitle;
+    private View onlineStatus;
+    private RecyclerView recyclerViewMessages;
+    private EditText editTextMessage;
+    private ImageView imageViewSendMessage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_chat);
+        iniViews();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void iniViews() {
+        textViewTitle = findViewById(R.id.textViewTitle);
+        onlineStatus = findViewById(R.id.onlineStatus);
+        recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+    }
+}
