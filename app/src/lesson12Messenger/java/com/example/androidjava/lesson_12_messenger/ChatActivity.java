ed7854817be184a495f791cb131b1dfb6bf1f501
Mark Klinitskii <mark23klin@mail.ru>
2025-11-24 11:31:32 +0300

    Lesson 12 - Messenger - ViewModelFactory

--- app/src/lesson12Messenger/java/com/example/androidjava/lesson_12_messenger/ChatActivity.java
+++ app/src/lesson12Messenger/java/com/example/androidjava/lesson_12_messenger/ChatActivity.java
@@ -7,15 +7,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androidjava.R;
+
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -33,6 +38,9 @@ public class ChatActivity extends AppCompatActivity {
     private String currentUserId;
     private String otherUserId;
 
+    private ChatViewModel viewModel;
+    private ChatViewModelFactory viewModelFactory;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +50,63 @@ public class ChatActivity extends AppCompatActivity {
         iniViews();
         currentUserId = getIntent().getStringExtra(EXTRA_CURRENT_USER_ID);
         otherUserId = getIntent().getStringExtra(EXTRA_OTHER_USER_ID);
+        viewModelFactory = new ChatViewModelFactory(currentUserId, otherUserId);
+        viewModel = new ViewModelProvider(this, viewModelFactory).get(ChatViewModel.class);
         messagesAdapter = new MessagesAdapter(currentUserId);
         recyclerViewMessages.setAdapter(messagesAdapter);
+        observeViewModel();
+
+        imageViewSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message message = new Message(
+                        editTextMessage.getText().toString().trim(),
+                        currentUserId,
+                        otherUserId
+                );
+                viewModel.sendMessage(message);
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getMessages().observe(this, new Observer<List<Message>>() {
+            @Override
+            public void onChanged(List<Message> messages) {
+                messagesAdapter.setMessages(messages);
+            }
+        });
+
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                 if (errorMessage != null) {
+                     Toast.makeText(ChatActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                 }
+            }
+        });
+
+        viewModel.getMessageSent().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean sent) {
+                if (sent) {
+                    editTextMessage.setText("");
+                }
+            }
+        });
+
+        viewModel.getOtherUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                 String userInfo = String.format("%s %s", user.getName(), user.getLastName());
+                 textViewTitle.setText(userInfo);
+            }
         });
     }
 
